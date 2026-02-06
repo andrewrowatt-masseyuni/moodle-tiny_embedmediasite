c1628f00570ff905bb4a2751c5eb382d7a0365a4
Andrew Rowatt <a.j.rowatt@massey.ac.nz>
2026-02-06 15:21:18 +1300

    Fixed some copilot issues.

--- classes/external/get_presentations.php
+++ classes/external/get_presentations.php
@@ -1,65 +1,98 @@
 <?php
 // This file is part of Moodle - http://moodle.org/
-// SPDX-License-Identifier: GPL-3.0-or-later
-// Copyright 2026, Andrew Rowatt <A.J.Rowatt@massey.ac.nz>.
-
-/**
- * Web service that exposes Mediasite presentation listing to AJAX callers.
- *
- * @package    tiny_embedmediasite
- */
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
 
 namespace tiny_embedmediasite\external;
 
-use core_external\external_api;
 use core_external\external_function_parameters;
-use core_external\external_multiple_structure;
 use core_external\external_single_structure;
+use core_external\external_multiple_structure;
+use core_external\external_api;
 use core_external\external_value;
-use tiny_embedmediasite\mediasite_client;
 
-// Ensure curl class is available outside of web context.
-global $CFG;
+// phpcs:ignoreFile moodle.Files.MoodleInternal.MoodleInternalGlobalState
+/* To resolved the Exception - Class "curl" not found issue */
 require_once($CFG->dirroot . '/lib/filelib.php');
 
+/**
+ * Implementation of web service tiny_embedmediasite_get_presentations
+ *
+ * @package    tiny_embedmediasite
+ * @copyright  2026 Andrew Rowatt <A.J.Rowatt@massey.ac.nz>
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
 class get_presentations extends external_api {
-
+    /**
+     * Describes the parameters for tiny_embedmediasite_get_presentations
+     *
+     * @return external_function_parameters
+     */
     public static function execute_parameters(): external_function_parameters {
         return new external_function_parameters([
-            'page' => new external_value(PARAM_INT, 'Which page of results to retrieve'),
+            'page' => new external_value(PARAM_INT, 'Page number'),
         ]);
     }
 
-    public static function execute(int $page): array {
-        $validated = self::validate_parameters(self::execute_parameters(), ['page' => $page]);
-        self::validate_context(\context_system::instance());
-        return mediasite_client::create()->load_page($validated['page']);
+    /**
+     * Implementation of web service tiny_embedmediasite_get_presentations
+     *
+     * @param int $page
+     */
+    public static function execute($page) {
+        // Parameter validation.
+        ['page' => $page] = self::validate_parameters(
+            self::execute_parameters(),
+            ['page' => $page]
+        );
+
+        // From web services we don't call require_login(), but rather validate_context.
+        $context = \context_system::instance();
+        self::validate_context($context);
+
+        $presentations = \tiny_embedmediasite\util::get_mediasite_presentations($page);
+
+        return $presentations;
     }
 
+    /**
+     * Describe the return structure for tiny_embedmediasite_get_presentations
+     *
+     * @return external_single_structure
+     */
     public static function execute_returns(): external_single_structure {
-        $item_shape = new external_single_structure([
-            'title'              => new external_value(PARAM_TEXT, 'Name of the presentation'),
-            'source'             => new external_value(PARAM_URL, 'Playback URL'),
-            'date'               => new external_value(PARAM_INT, 'Creation epoch'),
-            'date_formatted'     => new external_value(PARAM_TEXT, 'Formatted creation date'),
-            'author'             => new external_value(PARAM_TEXT, 'Owner name'),
-            'icon'               => new external_value(PARAM_URL, 'Mimetype icon path'),
-            'thumbnail'          => new external_value(PARAM_URL, 'Thumbnail proxy link', VALUE_OPTIONAL),
-            'thumbnail_width'    => new external_value(PARAM_INT, 'Thumb width px'),
-            'thumbnail_height'   => new external_value(PARAM_INT, 'Thumb height px'),
-            'mimetype'           => new external_value(PARAM_TEXT, 'Media type label'),
-            'duration'           => new external_value(PARAM_INT, 'Length in ms'),
-            'duration_formatted' => new external_value(PARAM_TEXT, 'Readable length'),
-        ]);
-
         return new external_single_structure([
-            'list'      => new external_multiple_structure($item_shape),
-            'manage'    => new external_value(PARAM_URL, 'Portal link', VALUE_OPTIONAL),
-            'nologin'   => new external_value(PARAM_BOOL, 'Login not needed flag'),
-            'norefresh' => new external_value(PARAM_BOOL, 'Refresh not needed flag'),
-            'nosearch'  => new external_value(PARAM_BOOL, 'Search not available flag'),
-            'page'      => new external_value(PARAM_INT, 'Current window index'),
-            'pages'     => new external_value(PARAM_INT, 'Total windows'),
+            'list' => new external_multiple_structure(
+                new external_single_structure([
+                    'title' => new external_value(PARAM_TEXT, 'Presentation title'),
+                    'source' => new external_value(PARAM_URL, 'Presentation source URL'),
+                    'date' => new external_value(PARAM_INT, 'Presentation creation date'),
+                    'date_formatted' => new external_value(PARAM_TEXT, 'Presentation creation date formatted'),
+                    'author' => new external_value(PARAM_TEXT, 'Presentation author'),
+                    'mimetype' => new external_value(PARAM_TEXT, 'Presentation mimetype'),
+                    'duration' => new external_value(PARAM_INT, 'Presentation duration in milliseconds'),
+                    'duration_formatted' => new external_value(PARAM_TEXT, 'Presentation duration formatted'),
+                    'description' => new external_value(PARAM_TEXT, 'Presentation description'),
+                    'thumbnail' => new external_value(PARAM_URL, 'Presentation thumbnail URL', VALUE_OPTIONAL),
+                ])
+            ),
+            'manage' => new external_value(PARAM_URL, 'Management URL', VALUE_OPTIONAL),
+            'nologin' => new external_value(PARAM_BOOL, 'No login required'),
+            'norefresh' => new external_value(PARAM_BOOL, 'No refresh'),
+            'nosearch' => new external_value(PARAM_BOOL, 'No search'),
+            'page' => new external_value(PARAM_INT, 'Current page number'),
+            'pages' => new external_value(PARAM_INT, 'Total pages'),
         ]);
     }
 }
--- thumbnail.php
+++ thumbnail.php
@@ -1,122 +1,117 @@
 <?php
 // This file is part of Moodle - http://moodle.org/
-// SPDX-License-Identifier: GPL-3.0-or-later
-// Copyright 2026, Andrew Rowatt <A.J.Rowatt@massey.ac.nz>.
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
 
 /**
- * Proxies thumbnail images from the Mediasite API so that browser
- * requests never need direct access to API credentials.
+ * Thumbnail proxy for Mediasite presentations
+ *
+ * This file acts as a proxy to load thumbnail images from Mediasite.
+ * It fetches the thumbnail using Mediasite API credentials and returns
+ * the image with the correct content type.
  *
  * @package    tiny_embedmediasite
+ * @copyright  2026 Andrew Rowatt <A.J.Rowatt@massey.ac.nz>
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
  */
 
-// This script lives at lib/editor/tiny/plugins/embedmediasite/ inside Moodle.
 require_once(__DIR__ . '/../../../../../config.php');
 require_once($CFG->libdir . '/filelib.php');
 
+use tiny_embedmediasite\util;
+
 require_login();
 
-/**
- * Terminate the request with an HTTP error.
- *
- * @param int    $code HTTP status code.
- * @param string $msg  Human-readable reason.
- * @return never
- */
-function bail(int $code, string $msg): void {
-    http_response_code($code);
-    echo $msg;
-    exit;
+$presentationid = required_param('id', PARAM_ALPHANUMEXT);
+
+// Get Mediasite configuration.
+$basemediasiteurl = get_config(util::M_COMPONENT, 'basemediasiteurl');
+$sfapikey = get_config(util::M_COMPONENT, 'sfapikey');
+$authorization = get_config(util::M_COMPONENT, 'authorization');
+
+if (empty($basemediasiteurl) || empty($sfapikey) || empty($authorization)) {
+    http_response_code(500);
+    die('Mediasite configuration is incomplete');
 }
 
-/**
- * Read a plugin setting or bail on missing config.
- *
- * @param string $key Setting name.
- * @return string The value.
- */
-function need_setting(string $key): string {
-    $val = get_config('tiny_embedmediasite', $key);
-    if ($val === false || $val === '') {
-        bail(500, "Missing configuration: {$key}");
-    }
-    return $val;
-}
+// Call the Mediasite API to get the thumbnail information.
+$endpoint = "https://{$basemediasiteurl}/Api/v1/Presentations('" . urlencode($presentationid) . "')/ThumbnailContent";
 
-/**
- * Make an authenticated GET to the Mediasite API.
- *
- * @param string $endpoint Full URL.
- * @param string $authval  Authorization header content.
- * @param string $apikeyval sfapikey header content.
- * @return array{body: string, info: array} Response body and curl info.
- */
-function api_get(string $endpoint, string $authval, string $apikeyval): array {
-    $transport = new curl();
-    $transport->setHeader([
-        'Content-Type: application/json',
-        'Authorization: ' . $authval,
-        'Accept: application/json',
-        'sfapikey: ' . $apikeyval,
-    ]);
-    $body = $transport->get($endpoint);
-    if ($transport->get_errno()) {
-        bail(502, 'Upstream request failed');
-    }
-    return ['body' => $body, 'info' => $transport->get_info()];
-}
-
-// --- Main flow ---
-
-$entryid   = required_param('id', PARAM_ALPHANUMEXT);
-$serverurl = need_setting('basemediasiteurl');
-$authval   = need_setting('authorization');
-$apikeyval = need_setting('sfapikey');
-
-// Step 1: ask the API for the thumbnail metadata of this presentation.
-$metaurl = 'https://' . rtrim($serverurl, '/')
-    . "/Api/v1/Presentations('" . urlencode($entryid) . "')/ThumbnailContent";
-
-$meta = api_get($metaurl, $authval, $apikeyval);
-
-if (($meta['info']['http_code'] ?? 0) != 200) {
-    bail(intval($meta['info']['http_code'] ?? 502), 'Upstream returned non-200');
-}
-
-$decoded = json_decode($meta['body'], true);
-
-if (empty($decoded['value'][0]['ThumbnailUrl'])) {
-    bail(404, 'No thumbnail available for this presentation');
-}
-
-$imgurl  = $decoded['value'][0]['ThumbnailUrl'];
-$imgmime = $decoded['value'][0]['ContentMimeType'] ?? 'image/jpeg';
-
-// Step 2: reject anything that isn't a raster image (SVG can carry scripts).
-$safetypes = ['image/jpeg', 'image/jpg', 'image/png', 'image/gif', 'image/webp'];
-if (!in_array($imgmime, $safetypes, true)) {
-    bail(415, 'Content type not permitted');
-}
-
-// Step 3: fetch the actual image bytes through an authenticated request.
-$imgtransport = new curl();
-$imgtransport->setHeader([
-    'Authorization: ' . $authval,
-    'sfapikey: ' . $apikeyval,
+$ch = new curl();
+$ch->setHeader([
+    'Content-Type: application/json',
+    "Authorization: {$authorization}",
+    'Accept: application/json',
+    "sfapikey: {$sfapikey}",
 ]);
-$imgbytes = $imgtransport->get($imgurl);
 
-if ($imgtransport->get_errno()) {
-    bail(502, 'Image fetch failed');
+$responseraw = $ch->get($endpoint);
+
+if ($ch->get_errno() !== 0) {
+    http_response_code(500);
+    die('Failed to fetch thumbnail information from Mediasite API');
 }
 
-$imginfo = $imgtransport->get_info();
-if (($imginfo['http_code'] ?? 0) != 200) {
-    bail(intval($imginfo['http_code'] ?? 502), 'Image upstream error');
+$info = $ch->get_info();
+
+if ($info['http_code'] != 200) {
+    http_response_code($info['http_code']);
+    die('Mediasite API returned error: ' . $info['http_code']);
 }
 
-// Step 4: relay the image with appropriate caching headers.
-header('Content-Type: ' . $imgmime);
-header('Content-Length: ' . strlen($imgbytes));
-header('Cache-Control: private, max-age=3600');
-echo $imgbytes;
+$response = json_decode($responseraw, true);
+
+if (!$response || empty($response['value'][0]['ThumbnailUrl'])) {
+    http_response_code(404);
+    die('Thumbnail not found');
+}
+
+$thumbnailurl = $response['value'][0]['ThumbnailUrl'];
+$contentmimetype = $response['value'][0]['ContentMimeType'] ?? 'image/jpeg';
+
+// Validate that the content type is an image to prevent content-type injection.
+// Note: SVG is excluded to prevent XSS attacks as SVG files can contain executable JavaScript.
+$allowedmimetypes = ['image/jpeg', 'image/jpg', 'image/png', 'image/gif', 'image/webp'];
+if (!in_array($contentmimetype, $allowedmimetypes)) {
+    http_response_code(400);
+    die('Invalid content type');
+}
+
+// Fetch the actual thumbnail image using Mediasite API credentials.
+$imagech = new curl();
+$imagech->setHeader([
+    "Authorization: {$authorization}",
+    "sfapikey: {$sfapikey}",
+]);
+
+$imagedata = $imagech->get($thumbnailurl);
+
+if ($imagech->get_errno() !== 0) {
+    http_response_code(500);
+    die('Failed to fetch thumbnail image');
+}
+
+$imageinfo = $imagech->get_info();
+
+if ($imageinfo['http_code'] != 200) {
+    http_response_code($imageinfo['http_code']);
+    die('Failed to fetch thumbnail image: ' . $imageinfo['http_code']);
+}
+
+// Return the image with the correct content type.
+header('Content-Type: ' . $contentmimetype);
+header('Content-Length: ' . strlen($imagedata));
+header('Cache-Control: private, max-age=3600'); // Private cache for 1 hour.
+
+echo $imagedata;
