93cfebb9966e9d8877ce5dffdb6b2a19edfd453f
Andrew Rowatt <a.j.rowatt@massey.ac.nz>
2026-02-17 15:11:22 +1300

    Bumped version ready for new release

--- version.php
+++ version.php
@@ -25,8 +25,8 @@
 defined('MOODLE_INTERNAL') || die();
 
 $plugin->component    = 'tiny_embedmediasite';
-$plugin->release      = '1.1';
-$plugin->version      = 2026011001;
+$plugin->release      = '1.2';
+$plugin->version      = 2026021701;
 $plugin->requires     = 2024100700;
 $plugin->supported    = [405, 501];
 $plugin->maturity     = MATURITY_STABLE;
